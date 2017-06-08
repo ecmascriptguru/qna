624dbfc5c16cd14266c178371a6c48abbe70b7ce
ecmascriptguru <ecmascript.guru@gmail.com>
2017-06-08 16:43:26 -0400

    something new

--- assets/js/qna.js
+++ assets/js/qna.js
@@ -11,13 +11,23 @@ let QuestionGenerator = (() => {
         $container = null;
 
     /**
+     * TEST FUNCTION THAT CHECKS IF IT DOES WORK OR NOT.
+     */
+    const renderSomething = () => {
+        $container.append(
+            $("<center><h2>Admin Library does work now.</h2></center>")
+        );
+    };
+
+    /**
      * Initialize this object with ID of element and API base URL
      * @param { string } containerID 
      * @param { string } apiBaseUrl 
      */
     const init = (containerID, apiBaseUrl) => {
-        $container = $(containerID);
+        $container = $(`#${containerID}`);
         _apiBaseUrl = apiBaseUrl;
+        renderSomething();
     };
 
     return {
@@ -33,13 +43,24 @@ let QuestionRenderer = (() => {
         $container = null;
 
     /**
+     * TEST FUNCTION THAT CHECKS IF IT DOES WORK OR NOT.
+     */
+    const renderSomething = () => {
+        $container.append(
+            $("<center><h2>Front end User Library does work now.</h2></center>")
+        );
+    };
+
+
+    /**
      * Initialize with Identity of HTML element and API base url
      * @param {string} id 
      * @param {string} url 
      */
     const init = (id, url) => {
-        $container = $(id);
+        $container = $(`#${id}`);
         _apiBaseUrl = url;
+        renderSomething();
     };
 
     return {
