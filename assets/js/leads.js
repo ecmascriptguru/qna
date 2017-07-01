7264d0f2fc02d2517d41d53c0d3756d9edd51e33
ecmascriptguru <ecmascript.guru@gmail.com>
2017-07-01 17:35:03 -0400

    hash feature created for test

--- assets/js/leads.js
+++ assets/js/leads.js
@@ -1,3 +1,21 @@
 (function(window, jQuery) {
-    QuestionRenderer.init("qna-container", QNAConfig.baseUrl(), 2);
+    let hash = null;
+
+    const hashChangeHandler = (curHash) => {
+        let pattern = /\d+/g;
+        let matches = (curHash || "").match(pattern);
+        let userId = 2;
+        if (matches && matches.length > 0) {
+            userId = parseInt(matches[0]);
+        }
+
+        QuestionRenderer.init("qna-container", QNAConfig.baseUrl(), userId);
+    }
+
+    window.setInterval(() => {
+        if (window.location.hash != hash) {
+            hash = window.location.hash;
+            hashChangeHandler(hash);
+        }
+    }, 500);
 })(window, $);
